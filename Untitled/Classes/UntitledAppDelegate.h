dc9d0f1f16ad292ebc758e397940c20937e982d2
Gaurav Garsa <gaurav90guru@gmail.com>
2011-03-16 23:41:58 +0530

    the first assignment

---
+++ Untitled/Classes/UntitledAppDelegate.h
@@ -0,0 +1,18 @@
+//
+//  UntitledAppDelegate.h
+//  Untitled
+//
+//  Created by Gaurav Garsa on 16/03/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UntitledAppDelegate : NSObject <UIApplicationDelegate> {
+    UIWindow *window;
+}
+
+@property (nonatomic, retain) IBOutlet UIWindow *window;
+
+@end
+

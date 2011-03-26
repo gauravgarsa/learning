3bcd032064dfd0f743fceadc304f307e048eb97c
Gaurav Garsa <gaurav90guru@gmail.com>
2011-03-26 21:41:14 +0530

    project with the custom class and its utilisation

---
+++ stanford 2/polygonShape.h
@@ -0,0 +1,17 @@
+//
+//  polygonShape.h
+//  stanford 2
+//
+//  Created by Gaurav Garsa on 23/03/11.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+
+@interface polygonShape : NSObject {
+@private
+    
+}
+
+@end
---
+++ stanford 2/polygonShape.m
@@ -0,0 +1,28 @@
+//
+//  polygonShape.m
+//  stanford 2
+//
+//  Created by Gaurav Garsa on 23/03/11.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import "polygonShape.h"
+
+
+@implementation polygonShape
+
+- (id)init {
+    if ((self = [super init])) {
+        // Initialization code here.
+    }
+    
+    return self;
+}
+
+- (void)dealloc {
+    // Clean-up code here.
+    
+    [super dealloc];
+}
+
+@end
